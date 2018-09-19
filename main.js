77241b38719e2c6fb3ec35097f47bdc66b379baa
amcleod72 <andrew@mcleods.net>
2018-09-19 18:08:46 +0100

    first commit

--- main.js
+++ main.js
@@ -1,53 +1,54 @@
 $('document').ready(function() {
     var sdk = new window.sfdc.BlockSDK();
     var crmIdField;
+    var modelAmp;
 
-    function debounce (func, wait, immediate) {
+    $.get("https://amcleod72.github.io/createtaskblock/amp.txt", function(data, status){
+        modelAmp = data;
+
+        sdk.getData(function(data) {
+            crmIdField = data.crmIdField;
+            fillSettings();
+        });
+    });
+
+    function debounce(func, wait, immediate) {
         console.log('Debouncing');
-    	var timeout;
-    	return function() {
-    		var context = this, args = arguments;
-    		var later = function() {
-    			timeout = null;
-    			if (!immediate) func.apply(context, args);
-    		};
-    		var callNow = immediate && !timeout;
-    		clearTimeout(timeout);
-    		timeout = setTimeout(later, wait);
-    		if (callNow) func.apply(context, args);
-    	};
+        var timeout;
+        return function() {
+            var context = this,
+                args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
     }
 
-    function fillSettings () {
-    	document.getElementById('crm-id-field').value = crmIdField;
+    function fillSettings() {
+        document.getElementById('crm-id-field').value = crmIdField;
     }
 
     function updateMe() {
-    	crmIdField = document.getElementById('crm-id-field').value;
-    	if (!crmIdField) {
-    		return;
-    	}
+        crmIdField = document.getElementById('crm-id-field').value;
+        if (!crmIdField) {
+            return;
+        }
 
         // Generate required AMPScript
-        var amp = '%%[\n';
-        amp += 'IF _messagecontext == 'SEND' THEN\n';
-        amp += 'SET @crmfield = AttributeValue("' + crmIdField + '")\n';
-        amp += 'ENDIF\n';
-        amp += '\n]%%';
-
-    	sdk.setContent(amp);
+        var amp = modelAmp.replace(/%%WhatId%%/img,crmIdField)
+        sdk.setContent(amp);
         //console.log('sdk content is : ' + sdk.getContent());
-    	sdk.setData({
-    		crmIdField: crmIdField
-    	});
+        sdk.setData({
+            crmIdField: crmIdField
+        });
     }
 
-    sdk.getData(function (data) {
-    	crmIdField = data.crmIdField;
-    	fillSettings();
-    });
-
-    document.getElementById('workspace').addEventListener("input", function () {
+    document.getElementById('workspace').addEventListener("input", function() {
         debounce(updateMe, 500)();
     });
 });
